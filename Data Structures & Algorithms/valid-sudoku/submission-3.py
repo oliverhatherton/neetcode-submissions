292b86f51e5691bed9c82d7a725b242d8bc585cb
Oliver Atherton <118294630+oliverhatherton@users.noreply.github.com>
2026-08-31 11:59:45 +0100

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxs = [set() for _ in range(9)]
+
+        for row_index, row in enumerate(board):
+            for col_index, cell in enumerate(row):
+                if cell == ".":
+                    continue
+
+                box_index = (row_index // 3) * 3 + (col_index // 3)
+
+                if cell in rows[row_index]:
+                    return False
+                
+                if cell in cols[col_index]:
+                    return False
+
+                if cell in boxs[box_index]:
+                    return False
+
+                rows[row_index].add(cell)
+                cols[col_index].add(cell)
+                boxs[box_index].add(cell)
+
+        return True
